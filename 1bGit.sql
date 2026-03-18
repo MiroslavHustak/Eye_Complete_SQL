f8c882af4ae9caa43bd82e42952eb1ae8f629f62
Mira <miroslav.hustak@atlas.cz>
2026-03-18 03:33:41 +0100

    19-03-2026

---
+++ 1bGit.sql
@@ -0,0 +1,29 @@
+/*
+1.b. K tabulce TabA vytvořte unikátní klíč na sloupec RC
+*/
+-- Tady asi také není co moc popisovat.
+-- Níže uvedený kód by zcela postačoval pro splnění zadání, .....
+/*
+USE Natalie
+GO
+
+ALTER TABLE dbo.TabA
+ADD CONSTRAINT UNIQUE_KEY_TabA_RC UNIQUE (RC);
+*/
+
+-- ... jenže se projevila paranoia funkcionálního programátora (tendence ověřovat kdeco): 
+
+USE Natalie;
+GO
+
+IF EXISTS (
+    SELECT 1 
+    FROM sys.tables 
+    WHERE [name] = 'TabA'
+)
+-- Pokud control statement (IF) má provést více jak jeden statement, BEGIN-END block je nutný
+BEGIN
+    ALTER TABLE dbo.TabA
+    -- DROP CONSTRAINT UNIQUE_KEY_TabA_RC --UNIQUE (RC);
+    ADD CONSTRAINT UNIQUE_KEY_TabA_RC UNIQUE (RC);
+END
