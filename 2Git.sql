de7ac52977dc821d3f0231458b166dd68f89892c
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2026-03-19 12:02:22 +0100

    Update 2Git.sql

--- 2Git.sql
+++ 2Git.sql
@@ -1,22 +1,3 @@
--- 2. Do tabulky TabA vložte skriptem 5 záznamů (hodnoty si můžete vybrat)
-
--- Níže uvedený kód by zcela postačoval pro splnění zadání, .....
-
-/*
-USE Natalie;
-GO
-
-INSERT INTO TabA (Jmeno, Prijmeni, RC, DatumNarozeni)
-VALUES
-('Floor', 'Jansen', '8102216847', '1981-02-21'),  
-('Tuomas','Holopainen','7612253089', '1976-12-25'), 
-('Emppu', 'Vuorinen', '7806241572', '1978-06-24'),  
-('Marco', 'Hietala', '6601125396', '1966-01-12'),   
-('Jukka', 'Nevalainen','7804219264', '1978-04-21'); 
-*/
-
--- ... jenže se projevila paranoia funkcionálního programátora (tendence ověřovat kdeco): 
-
 USE Natalie;
 GO
 
@@ -26,7 +7,7 @@ IF EXISTS (
     WHERE [name] = 'TabA'
 )
 
-INSERT INTO TabA (Jmeno, Prijmeni, RC, DatumNarozeni) -- V tomto případě by nemělo být riziko SQL injection (TODO řešit pouze v ADO.NET, pokud se k tomu dostanu)
+INSERT INTO TabA (Jmeno, Prijmeni, RC, DatumNarozeni) 
 VALUES
 ('Floor', 'Jansen', '8102216847', '1981-02-21'),  
 ('Tuomas','Holopainen','7612253089', '1976-12-25'), 
